5f6b0bb83a1e7be50843420bb971880725c8dc77
Thanh Dat <truongthanhdat0305@gmail.com>
2025-03-24 01:16:19 +0700

    pull cnf

--- src/main.py
+++ src/main.py
@@ -12,8 +12,6 @@ def main():
     literalDict = getDictOfLiterals(clauses)
     writeCNF(clauses, literalDict, intersections)
     writeLiterals(clauses)
-    for intersection in intersections:
-        print(f"{intersection[0][0].getIndex()} to {intersection[0][1].getIndex()} and {intersection[1][0].getIndex()} to {intersection[1][1].getIndex()} are intersections")
 
 if __name__ == '__main__':
     main()
